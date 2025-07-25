ad282358d21bb90898777344bf47c56ee4349bb7
otavio <otaviotemoteo@gmail.com>
2025-07-25 09:05:58 -0300

    login using db connection

--- src/auth/signin.py
+++ src/auth/signin.py
@@ -1,13 +1,14 @@
-users = {
-    "gerencia": {"password": "1234", "type": "management"},
-    "vendedor": {"password": "abcd", "type": "seller"}
-}
+from src.database.connection import get_connection
 
 def login_verification(username, password):
-    if username in users:
-        if users[username]["password"] == password:
-            return users[username]["type"]
-        else:
-            return "Senha incorreta!"
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    query = "SELECT * FROM usuarios WHERE username = %s AND password = %s"
+    cursor.execute(query, (username, password))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if user:
+        return user["tipo"]
     else:
-        return "Usuário não encontrado!"
+        return "Usuário ou senha incorretos!"
